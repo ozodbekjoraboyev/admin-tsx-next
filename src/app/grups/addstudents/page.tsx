80c24743aea9fe5bebbbc0b448c4b462dd99510a
unknown <baxtiyor.gazimboyev@gmail.com>
2025-03-17 14:13:11 +0300

    ddd

---
+++ src/app/grups/addstudents/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { Button, Drawer, Form, Input, Radio, Space } from "antd";
+import React, { useState } from "react";
+import { studentData } from "@/constants/data";
+import { useGlobalStor } from "@/store/my-stor-zustand";
+
+function AddStudents() {
+  const state = useGlobalStor();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+    form.resetFields();
+  };
+
+  return (
+    <div>
+      <Button onClick={showDrawer} type="primary" className=" z-50 absolute">
+        + Qo‘shish
+      </Button>
+
+      <Drawer
+        title="🆕 Yangi Talaba Qo‘shish"
+        onClose={onClose}
+        open={open}
+        width={400}
+      >
+        <Form
+          onFinish={(value) => {
+            setLoading(true);
+            console.log(value);
+            const new_student = state.students.concat({
+              id: Math.floor(Math.random() * 10000),
+              ...value,
+            });
+            useGlobalStor.setState({
+              students: new_student,
+            });
+
+            setTimeout(() => {
+              setLoading(false);
+              onClose();
+            }, 500);
+          }}
+          form={form}
+          layout="vertical"
+          className="space-y-3"
+        >
+          <Form.Item
+            name="name"
+            label=" Ism"
+            rules={[{ required: true, message: "Ismni kiriting!" }]}
+          >
+            <Input placeholder="Ismni kiriting" />
+          </Form.Item>
+
+          <Form.Item
+            name="age"
+            label=" Yosh"
+            rules={[{ required: true, message: "Yoshni kiriting!" }]}
+          >
+            <Input type="number" placeholder="Yoshni kiriting" />
+          </Form.Item>
+
+          <Form.Item
+            name="gender"
+            label=" Jins"
+            rules={[{ required: true, message: "Jinsni tanlang!" }]}
+          >
+            <Radio.Group buttonStyle="solid">
+              <Radio.Button value="male">Erkak</Radio.Button>
+              <Radio.Button value="female">Ayol</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item
+            name="job"
+            label=" Kasb"
+            rules={[{ required: true, message: "Kasbni kiriting!" }]}
+          >
+            <Input placeholder="Kasbni kiriting" />
+          </Form.Item>
+
+          <Space className="w-full flex justify-end">
+            <Button onClick={onClose}>Bekor qilish</Button>
+            <Button loading={loading} type="primary" htmlType="submit">
+              {loading ? "Jonatilmoqda..." : "✅ Saqlash"}
+            </Button>
+          </Space>
+        </Form>
+      </Drawer>
+    </div>
+  );
+}
+
+export default AddStudents;
--- src/app/grups/page.tsx
+++ src/app/grups/page.tsx
@@ -1,42 +1,52 @@
 "use client";
-import { Button, Drawer, Form, Input, Table } from "antd";
+import { columns as defaultColumns } from "@/constants";
+import { studentData } from "@/constants/data";
+import { Table, Button } from "antd";
 import React, { useState } from "react";
+import AddStudents from "./addstudents/page";
+import { useGlobalStor } from "@/store/my-stor-zustand";
 
-function StudentGroups() {
-  const [open, setOpen] = useState(false);
-  const showDrawer = () => {
-    setOpen(true);
+function Grups() {
+  const state = useGlobalStor();
+
+  const handleDelete = (id: any) => {
+    const deletee = state.students.filter((item) => item.id !== id);
+    useGlobalStor.setState({
+      groups: deletee,
+    });
   };
 
-  const onClose = () => {
-    setOpen(false);
-    // form.resetFields();
-  };
-  return (
-    <div className="p-4 absolute pl-[220px] pr-10 w-full container m-auto">
-      <div>
-        <h1>Grups</h1>
-        <Button onClick={showDrawer} className="absolute z-50">
-          +Qo{"'"}shish
+  const columns = [
+    ...defaultColumns,
+    {
+      title: "Action",
+      key: "action",
+      render: (record: any) => (
+        <Button
+          type="primary"
+          className=" z-50 absolute"
+          danger
+          onClick={() => handleDelete(record.id)}
+        >
+          O‘chirish
         </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div className="absolute pl-[220px] pr-10 w-full container m-auto">
+      <div className="flex justify-between items-center p-4 bg-white shadow-md rounded-lg">
+        <h1 className="text-xl font-semibold">📚 Grups</h1>
+
+        <AddStudents />
       </div>
-      <Drawer
-        title="🆕 Yangi Guruh Qo'shish"
-        onClose={onClose}
-        open={open}
-        width={400}
-      >
-        <Form>
-          <Form.Item name="Ism" label="Ism" rules={[{ required: true }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item rules={[{ required: true }]}>
-            <Button htmlType="submit">Saqlash</Button>
-          </Form.Item>
-        </Form>
-      </Drawer>
+
+      <div className="w-full">
+        <Table dataSource={state.students}  columns={columns} rowKey="id" />
+      </div>
     </div>
   );
 }
 
-export default StudentGroups;
+export default Grups;
